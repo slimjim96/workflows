3a32151f39530aa61de9abb37c48791bfb767cb2
Jim Valenti <jjvalenti@paychex.com>
2017-01-03 16:02:22 -0500

    Concatenated javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
-var coffeeSources = ['components/coffee/tagline.coffee' ]
+var coffeeSources = ['components/coffee/tagline.coffee' ];
+var jsSources = ['components/scripts/rclick.js',
+                'components/scripts/pixgrid.js',
+                'components/scripts/tagline.js',
+                'components/scripts/template.js'];
+
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
         .pipe(coffee({bare: true})
@@ -10,4 +16,8 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('components/scripts'))
 });
 
+gulp.task('js', function() {
+    gulp.src(jsSources).pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+})
 
